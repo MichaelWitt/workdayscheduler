f3c55a1047aa319728a11be70b5a294676410ead
Annema Cozmoz <mikewittorp@gmail.com>
2020-09-30 20:27:37 -0400

    updated the container in html with rows, columns, text area, and button, as well as stylizing it with the css

---
+++ Develop/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    /*
+     Pseudocode
+
+     center with green when time 9-5 and grey when not 9-5 and red when current hour
+     When clicked, text entered into container is saved on local storage. Unllick to edit.
+    
+     get current hour from moment
+     function that loops over time block in html
+     get the hour of block from timeframe block
+     if block hour less than current hour
+     add past class 
+     else if black hour = current hour 
+     add present class
+     remove past class
+     else if block hour greater than current hour
+     add future class
+     remove past class
+     remove present class
+
+     call function on pageload
+     add to set interval to run every 5 minutes
+
+     on page load, load any save data from local storage
+     event listener clicking save button
+     which will save text area to local storage
+     set value of what we get from local storage to text area
+     write a loop or just paste all: $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    
+     */
+
+
+
+
+
+
+
+
+});
