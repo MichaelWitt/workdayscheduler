e10120a17c85547eec8bb1a9a6f05e2794ad476d
Annema Cozmoz <mikewittorp@gmail.com>
2020-10-01 10:59:15 -0400

    updated all hours with local storage getitems

--- Develop/script.js
+++ Develop/script.js
@@ -32,13 +32,6 @@ $(document).ready(function () {
 
 
 
-    // $(".saveBtn").on("click", function () {
-    //     // get nearby values
-    //     var value = $(this).siblings(".description").val();
-    //     var time = $(this).parent().attr("id");
-    //     // save in localStorage
-    //     localStorage.setItem(time, value);
-    // });
 
     $('.saveBtn').on('click', function () {
         var value = $(this).siblings('.description').val();
@@ -47,7 +40,15 @@ $(document).ready(function () {
     });
 
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 
 
 
